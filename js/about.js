66df677aa303d0626cd85cf20f37cc065708b279
panhaanosu <49329136+panhaanosu@users.noreply.github.com>
2020-02-07 03:56:07 -0800

    Create about.js

---
+++ js/about.js
@@ -0,0 +1,27 @@
+/* SOURCES: 
+  https://www.w3schools.com/bootstrap4/bootstrap_ref_js_collapse.asp
+  https://www.w3schools.com/bootstrap4/bootstrap_ref_js_carousel.asp
+*/
+
+$(document).ready(function () {
+  $(".navbar-toggler").click(function () {
+    $(".collapse").collapse('toggle');
+  });
+
+  $('#myCarouselSlide').carousel({
+    interval: 3000
+  });
+
+  $('.slide1').click(function () {
+    $('#myCarouselSlide').carousel(0);
+  });
+
+  $('.slide2').click(function () {
+    $('#myCarouselSlide').carousel(1);
+  });
+
+  $('.slide3').click(function () {
+    $('#myCarouselSlide').carousel(2);
+  });
+
+});
