12655f286a7d9def73ec959e8c65f91b521660aa
anp-tech <49329136+panhaanosu@users.noreply.github.com>
2020-02-09 06:45:03 -0800

    Update employee.js

--- js/employee.js
+++ js/employee.js
@@ -1,15 +1,6 @@
 /* SOURCES: 
-  https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_checkbox_checked2
   https://www.w3schools.com/jsref/tryit.asp?filename=tryjsref_table_deleterow
 */
-
-function check() {
-    document.getElementById("hasBeenMade").checked = true;
-  }
-  
-function uncheck() {
-    document.getElementById("hasBeenMade").checked = false;
-  }
 
 function toDelete() {
     document.getElementById("deleteByRow").deleteRow(0);
