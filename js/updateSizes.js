9779e63d0d11cf66d7917c46a399c96bdd866950
anp-tech <49329136+panhaanosu@users.noreply.github.com>
2020-03-01 19:40:53 -0800

    Create updateSizes.js

---
+++ js/updateSizes.js
@@ -0,0 +1,179 @@
+
+//A global var to hold the pizza sizes as returned by GetSizesAjax
+var SizesArr = [];
+
+window.onload = async ()=>{
+    SizesArr = await getSizesAjax();
+    SizesArr = JSON.parse(SizesArr)
+    BuildSizesTable();
+}
+
+//will make an ajax request to query for the list of pizza sizes. We make this request again whenever an item is added to the table.
+ async function getSizesAjax() {
+    //for now we just return the global array of sizes.
+    return await $.ajax({
+        url: "server/getSizes.php",
+        type: "GET"
+    });
+}
+
+//takes the data stored in the ToppinsList global array, and builds it into the sizes table.
+function BuildSizesTable(){
+  $("#sizesTableBody").html(""); 
+
+  $("#sizesTable").find('tbody')
+    .append($('<tr>')
+        .append($('<th>')
+
+        .text("Size Name"))
+        .append($('<th>')
+
+        .text("Price"))
+        .append($('<th>')
+        .text("Update"))
+        .append($('<th>')
+        .text("Delete"))
+    )
+ 
+    console.log(SizesArr)
+  for(var i in SizesArr){
+      var top = SizesArr[i];
+      //console.log(top)
+      $("#sizesTable").find('tbody')
+        .append($('<tr>')
+            .append($('<td>')
+                .append($('<span>')
+                    .text(top.sizeName)
+                    .attr('id', top.sizeID + "_name")
+                )
+            )
+            .append($('<td>')
+                .append($('<span>')
+                    .text(top.price)
+                    .attr('id', top.sizeID + "_price")
+                )
+            )
+            .append($('<td>')
+                .append($('<button>')
+                    .text("Update")
+                    .attr('id', top.sizeID + "_update")
+                    .attr('class', "btn btn-secondary")
+                    .attr('style', "background:#B43A10")
+                    .attr('onclick', "UpdateButtonClickAction("+top.sizeID+")")
+                )
+            )
+            .append($('<td>')
+                .append($('<button>')
+                    .text("Delete")
+                    .attr('id', top.sizeID + "_delete")
+                    .attr('class', "btn btn-secondary")
+                    .attr('style', "background:#B43A10")
+                    .attr('onclick', "DeleteButtonClickAction("+top.sizeID+")")
+                )
+            )
+            .attr('id', top.sizeID)
+        ).attr('id', "sizesTableBody");
+  }
+}
+
+//Makes an ajax post to add a new size from the input fields. After we update the db, we rebuild the table
+async function AddSizeAjax(Name, Price){
+    data = {sizeName:Name, price:Price};
+    let result;
+   try{
+      result = await $.ajax({
+         url:"server/addSizes.php",
+         type:"POST",
+         data:data
+      });
+      return result;
+   }
+   catch(error){
+       console.error(error);
+   }
+}
+
+
+//makes ajax request to update a size when user clicks the update button on the modal
+function UpdateSizeAjax(){
+
+}
+
+
+//function gets called when user clickes the update button in the Modal
+async function SubmitSizeChanges(){
+
+    $('#updateSizeModal').modal('hide');
+
+    //make ajax request to update the db. Then rebuild the table with the new data.
+    await UpdateSizeAjax();
+
+    SizesArr = await GetSizesAjax();
+
+    await BuildSizesTable();
+;
+}
+
+
+//function is called when a update button is clicked in the sizes table.
+async function UpdateButtonClickAction(rowId){
+     $('#updateSizeModal').modal('show');
+
+     let nameRowId = rowId + "_name"
+     let name = document.getElementById(nameRowId).innerText;
+
+     let descriptionRowId = rowId + "_price"
+     let description = document.getElementById(descriptionRowId).innerText;
+
+     $("#updateName").val(name)
+     $("#updateDescription").val(description)
+     $("#updateId").val(rowId) //hiden field to track the id of the updated size
+
+
+}
+
+
+//function is called when a delete button is clicked in the sizes table. Then we requery for the table data and build it
+async function DeleteButtonClickAction(rowId){
+ $('#confirmDeleteModal').modal('show');
+
+ //set the hiden id of the value to be deleted
+
+}
+
+
+//makes call to ajax function to delete the size. Also rebuilds the table.
+async function DeleteSize(){
+     $('#confirmDeleteModal').modal('hide');
+   await DeleteSizeAjax();
+   SizesArr = await GetSizesAjax();
+   await BuildSizesTable();
+}
+
+
+//makes an ajax request to delete a size
+function DeleteSizeAjax(){
+
+}
+
+//This function is called when the Add button is clicked
+async function AddSizeButtonClickAction(){
+    let name = $("#sizeName").val();
+    if(name == undefined || name == null || name == "") return;
+
+    let p = $("#sizePrice").val();
+    if(p == undefined || p == null || p == "") return;
+
+    await AddSizeAjax(name, p);
+    
+    //refresh the page so the updated table gets built
+    window.location.reload(); FIXME
+}
+
+class Size{
+    constructor(id, name, p){
+        this.Id = id;
+        this.Name = name;
+        this.Description = p;
+    }
+}
