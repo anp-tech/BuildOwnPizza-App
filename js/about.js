c320eda599f3137b457ec1d5a818ae3ff0d97e89
anp-tech <49329136+panhaanosu@users.noreply.github.com>
2020-08-20 17:38:24 -0700

    updated about.html and used env variables for DB credentials

--- js/about.js
+++ js/about.js
@@ -1,27 +1,5 @@
-/* SOURCES: 
-  https://www.w3schools.com/bootstrap4/bootstrap_ref_js_collapse.asp
-  https://www.w3schools.com/bootstrap4/bootstrap_ref_js_carousel.asp
-*/
-
 $(document).ready(function () {
   $(".navbar-toggler").click(function () {
     $(".collapse").collapse('toggle');
   });
-
-  $('#myCarouselSlide').carousel({
-    interval: 2500
-  });
-
-  $('.slide1').click(function () {
-    $('#myCarouselSlide').carousel(0);
-  });
-
-  $('.slide2').click(function () {
-    $('#myCarouselSlide').carousel(1);
-  });
-
-  $('.slide3').click(function () {
-    $('#myCarouselSlide').carousel(2);
-  });
-
 });
--- server/getDBConnection.php
+++ server/getDBConnection.php
@@ -1,18 +1,25 @@
 <?php
 
-//The function to generate the connection string to the CS340 database
- function GetDBConnection(){
-     $servername = "classmysql.engr.oregonstate.edu";
-     $username = "cs340_hoglundb";
-     $password = "6406";
-     $dbname = "cs340_hoglundb";
-     // Create connection
-     $conn = new mysqli($servername, $username, $password, $dbname);
-     // Check connection
-     if ($conn->connect_error) {
-         die("Connection failed: " . $conn->connect_error);
-     }
-     return $conn;
-  }
+require_once realpath(__DIR__ . '/vendor/autoload.php');
 
+$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
+$dotenv->load();
+
+//The function to generate the connection string to the CS340 database
+function GetDBConnection() {
+    $servername = $_ENV['SERVERNAME'];
+    $username = $_ENV['DB_USERNAME'];
+    $password = $_ENV['DB_PASSWORD'];
+    $dbname = $_ENV['DB_NAME'];
+    
+    // Create connection
+    $conn = new mysqli($servername, $username, $password, $dbname);
+     
+    // Check connection
+    if ($conn->connect_error) {
+        die("Connection failed: " . $conn->connect_error);
+    }
+    return $conn;
+} 
+  
 ?>
