f69290e447991c5a0669aba886504d921e4f2d6f
anp-tech <49329136+panhaanosu@users.noreply.github.com>
2020-08-22 23:06:02 -0700

    env variables for JawsDB

--- server/getDBConnection.php
+++ server/getDBConnection.php
@@ -1,25 +1,23 @@
 <?php
 
-require_once realpath(__DIR__ . '/vendor/autoload.php');
-
-$dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__);
-$dotenv->load();
-
-//The function to generate the connection string to the CS340 database
+//The function to generate the connection string to the JawsDB
 function GetDBConnection() {
-    $servername = $_ENV['SERVERNAME'];
-    $username = $_ENV['DB_USERNAME'];
-    $password = $_ENV['DB_PASSWORD'];
-    $dbname = $_ENV['DB_NAME'];
-    
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+
+    $servername = $dbparts['host'];
+    $username = $dbparts['user'];
+    $password = $dbparts['pass'];
+    $dbname = ltrim($dbparts['path'],'/');
+
     // Create connection
     $conn = new mysqli($servername, $username, $password, $dbname);
-     
+    
     // Check connection
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
     return $conn;
-} 
+}
   
 ?>
